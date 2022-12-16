ad815686e0a06fe10c7094cdfc8c6a9f29c93dd3
Axionauta <daniln2209@gmail.com>
2022-12-16 01:47:20 -0300

    feat: add basic global styles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button {
+    border: none;
+    cursor: pointer;
+    text-align: center;
+  }
+
+  input {
+    border: none;
+    outline: none;
+  }
+`
