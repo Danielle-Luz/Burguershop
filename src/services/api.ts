a5aa7136dfb177e99fbc12684cb4e8af7f08146a
Axionauta <daniln2209@gmail.com>
2022-12-16 02:03:23 -0300

    feat: add and configure axios instance

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,1 +1,8 @@
-export const api = 
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "https://hamburgueria-kenzie-v2.herokuapp.com/",
+  timeout: 6000
+});
+
+api.defaults.headers.common["Authorization"] = localStorage.getItem("@token");
