058547c8e295b724c18f36ab4b4fba89c79733c4
Axionauta <daniln2209@gmail.com>
2022-12-16 01:30:12 -0300

    feat: add colors on theme object

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    brand: {
+      primary: ["#EB5757", "F95D5D"],
+      secondary: ["#27AE60", "#93D7AF"]
+    },
+    gray: ["#333333", "#828282", "#E0E0E0", "#F5F5F5"],
+    status: {
+      negative: "#E60000",
+      warning: "#FFCD07",
+      sucess: "#168821",
+      information: "#155BCB"
+    }
+  }
+}
