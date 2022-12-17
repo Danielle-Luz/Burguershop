31c90332a74465c099e1a2f6d5fde73f978a0f43
Axionauta <daniln2209@gmail.com>
2022-12-17 01:35:20 -0300

    feat: move form content from component

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,20 @@
-import { FormUser } from "../../components/FormUser";
+import { InputWrapper } from "../../components/FormUser/InputWrapper";
+import { FormUserStyled } from "../../components/FormUser/styles";
 import { SiteInfo } from "../../components/SiteInfo";
+import { TitleStyled } from "../../components/Texts/styles";
 import { LoginStyled } from "./styles";
 
 export function Login() {
   return (
     <LoginStyled>
       <SiteInfo />
-      <FormUser />
+      <FormUserStyled>
+        <TitleStyled tag="h3">Login</TitleStyled>
+        <form>
+          <InputWrapper label="Email" type="email" />
+          <InputWrapper label="Senha" type="password" />
+        </form>
+      </FormUserStyled>
     </LoginStyled>
   );
 }
