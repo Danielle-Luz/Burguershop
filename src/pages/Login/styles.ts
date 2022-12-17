2947b4fa6ab2b61287e503f187370d3ebd887f7b
Axionauta <daniln2209@gmail.com>
2022-12-17 01:41:32 -0300

    feat: add login styled styles

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,5 +1,19 @@
 import styled from "styled-components";
 
 export const LoginStyled = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 15px;
+  padding: 20px;
 
+  & > article {
+    max-width: 452px;
+    width: 100%;
+  }
+
+  @media(min-width: 875px) {
+    flex-direction: row-reverse;
+  }
 `
