59b62b433e20fc50faf4d80d03dd93edf5eadae2
Axionauta <daniln2209@gmail.com>
2022-12-18 15:46:04 -0300

    feat: add mediaa querie on header

--- src/pages/Dashboard/Header/styles.ts
+++ src/pages/Dashboard/Header/styles.ts
@@ -8,9 +8,21 @@ interface iHeaderStyledProps {
 export const HeaderStyled = styled.header`
   align-self: flex-start;
   background-color: ${({theme}: iHeaderStyledProps) => theme.colors.gray[3]};
-  display: flex;
-  justify-content: space-between;
   position: sticky;
   top: 0;
   width: 100%;
+  
+  & > div {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+
+  @media (max-width: 650px) {
+    & > div {
+      align-items: center;
+      flex-direction: column;
+      gap: 20px;
+    }
+  }
 `;
