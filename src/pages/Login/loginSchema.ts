e1f2c5d09807c5164346d6c9dc238be4a83571f0
Axionauta <daniln2209@gmail.com>
2022-12-18 12:35:24 -0300

    fix: modifies schema messages

--- src/pages/Login/loginSchema.ts
+++ src/pages/Login/loginSchema.ts
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
 export const loginSchema = yup.object().shape({
-  email: yup.string().required("Insira um email"),
-  password: yup.string().required("Insira uma senha")
+  email: yup.string().required("O email é obrigatório").email("Insira um email válido"),
+  password: yup.string().required("A senha é obrigatória").min(6, "A senha deve ter no mínimo 6 caracteres")
 });
