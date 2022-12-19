8dc4eafd3c90c083395c4263a7f26c39c52b7f95
Axionauta <daniln2209@gmail.com>
2022-12-18 21:51:39 -0300

    feat: implements cart total

--- src/components/Modal/CartTotal/index.tsx
+++ src/components/Modal/CartTotal/index.tsx
@@ -1,9 +1,15 @@
+import { ButtonStyled } from "../../Button/styles";
+import { TextStyled } from "../../Texts/styles";
 import { CartTotalStyled } from "./styles";
 
 export function CartTotal() {
   return (
     <CartTotalStyled>
-      
+      <div>
+        <TextStyled tag="p" gray={0} weight={600}>Total</TextStyled>
+        <TextStyled tag="p" gray={1} weight={600}>Total</TextStyled>
+      </div>
+      <ButtonStyled type="button" buttonStyle="gray">Remover todos</ButtonStyled>
     </CartTotalStyled>
   );
 }
