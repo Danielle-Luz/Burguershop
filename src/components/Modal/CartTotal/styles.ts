743b3ea4de0b40fb2043c19bf099c2b164178387
Axionauta <daniln2209@gmail.com>
2022-12-18 21:55:24 -0300

    feat: add cart total style

--- src/components/Modal/CartTotal/styles.ts
+++ src/components/Modal/CartTotal/styles.ts
@@ -1,5 +1,22 @@
 import styled from "styled-components";
+import { iTheme } from "../../../styles/theme";
+
+interface CartTotalStyledProps {
+  theme: iTheme;
+}
 
 export const CartTotalStyled = styled.article`
-
+  align-items: center;
+  border-top: 2px solid ${({theme}: CartTotalStyledProps) => theme.colors.gray[2]};
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  padding-top: 15px;
+  width: 100%;
+  
+  div {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
 `
